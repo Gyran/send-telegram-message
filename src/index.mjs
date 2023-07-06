9efcca0520f2ed9accd4fd4b4a36a275592bc8bc
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2023-07-06 14:25:22 +0200

    allow options

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,9 @@
-const sendTelegramMessage = async (botToken, chatId, message) => {
+const sendTelegramMessage = async (botToken, chatId, message, options = {}) => {
   const baseUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
   const data = {
     chat_id: chatId,
     text: message,
+    ...options,
   };
 
   const response = await fetch(baseUrl, {
